dd0fcfa1f1889580c23c63c7c0ff0cecb1483b15
SC-Goestowork <65223940+SC-Goestowork@users.noreply.github.com>
2022-03-18 10:49:33 +0800

    Update index.ts
    solution to test 1

--- #1/index.ts
+++ #1/index.ts
@@ -1,10 +1,11 @@
 export default class<TInput extends Array<any> = Array<any>, TOutput = any> {
   constructor(private handler: (...args: TInput) => Promise<TOutput>, private timeout?: number) {
-    // do something
+    let TInput: string[] = ['hello', 'world', 'everybody'];
   }
 
   async exec(...args: TInput): Promise<TOutput> {
-    // do something
-    return {} as TOutput;
+    let TOutput: string[] = [];
+    TOutput.push(TInput.toString());
+    return TOutput;
   }
 }
