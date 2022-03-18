1dc4176f4107a6a8b613e89ab2ac5b01a21d30a4
SC-Goestowork <65223940+SC-Goestowork@users.noreply.github.com>
2022-03-18 11:27:21 +0800

    Update index.ts
    solution to test 2

--- #2/index.ts
+++ #2/index.ts
@@ -5,8 +5,9 @@
  * @param endDateTime 
  */
 export function availability(startDateTime: Date, endDateTime: Date): number {
-  // do something
-  return 0;
+  const Day_Conversion = 1000 * 60 * 60 * 24;
+  
+  return Math.floor((endDateTime - startDateTime) / Day_Conversion);
 }
 
 /**
@@ -17,6 +18,21 @@ export function availability(startDateTime: Date, endDateTime: Date): number {
  * @param endDateTime 
  */
 export function outages(startDateTime: Date, endDateTime: Date): { type: 'PARTIAL' | 'MAJOR', timestamp: Date, duration: number }[] {
-  // do something
-  return [];
+  const Day_Conversion = 1000 * 60 * 60 * 24;
+  
+  let delayStatus: string = '';
+  let partialDayCutoff: number = 15;
+  let startDay: number =  Date.UTC(startDateTime.getFullYear(), startDateTime.getMonth(), startDateTime.getDate());
+  let endDay: number = Date.UTC(endDateTime.getFullYear(), endDateTime.getMonth(), endDateTime.getDate());
+  let delayDays: number = = Math.floor((endDay - startDay) / Day_Conversion);
+  let timestamp: Date = endDateTime - startDateTime;
+  
+  if (delayDays <== partialDayCutoff){
+    delayStatus = 'PARTIAL';
+  }
+  else{
+    delayStatus = 'MAJOR';
+  }
+  
+  return new Array(delayStatus, timestamp, delayDays);
 }
