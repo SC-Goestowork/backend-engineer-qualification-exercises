aad10fb512e65384fed531340d38c64ddec9b26b
SC-Goestowork <65223940+SC-Goestowork@users.noreply.github.com>
2022-03-18 14:53:57 +0800

    Update index.ts
    solution to test 3

--- #3/index.ts
+++ #3/index.ts
@@ -6,6 +6,33 @@
  * @param data 
  */
 export default function logest(ys: number[]): number {
-  // do something
-  return 0;
+  let result: number = 0;
+  let ysLength: number = ys.lenght;
+  let isBelowZero: number = 0;
+  let sumX: number = 0;
+  let sumY: number = 0;
+  let sumXY: number = 0;
+  let sumXX: number = 0;
+  let sumYY: number = 0;
+  let defalutValues: number[] = [1,2,3,4,5];
+  
+  for(var i = 0; i < ys.length; i++){ 
+    if (ys[i] <= isBelowZero){
+       result = NaN;
+       break;
+    }
+    else{
+            sumX += i + 1;
+            sumY += ys[i];
+            sumXY += (sumX*ys[i]);
+            sumXX += (sumX*sumX);
+            sumYY += (ys[i]*ys[i]);
+    }
+  }
+  
+  if (result != NaN){
+    result = (ysLength * sumXY - sumX * sumY) / (ysLength * sumXX - sumX * sumX);
+  }
+  
+  return result;
 }
